b85e287e0db64d88c79f275000516c214eb6893a
Mohammed Razeen <mohammedrazeen716@gmail.com>
2025-10-01 01:53:22 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('.food-slider').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+    })
+});
+//   var hour = 
+//   var minute = 9;
+//   var second = 60;
+//    setInterval(function() {
+//        if (minute == 0 && second == 1) {
+//            document.getElementsByClassName("timer").innerHTML = 
+//        }
+//    })
